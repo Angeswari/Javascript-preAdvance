dafcabe6d2601728bde1486c00a246dc636135c6
Angeswarig <angeswarig97@gmail.com>
2021-08-13 18:55:54 +0530

    advanish added

---
+++ pre-Advance/setTimeInterval.js
@@ -0,0 +1,7 @@
+setTimeout(function () {
+        console.log("Hello")
+    }, 1000); // Execute something() 1 second later.
+    
+setInterval(function () {
+        console.log("Hi");
+    }, 2000); // Execute somethingElse() every 2 seconds.
